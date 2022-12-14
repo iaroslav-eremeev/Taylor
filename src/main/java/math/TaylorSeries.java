2329846d78c597ef6d25f85b54df5fa104609e37
iaroslav.eremeev <eremeev.pt@gmail.com>
2022-12-14 11:29:37 +0200

    Add arctg method

--- src/main/java/math/TaylorSeries.java
+++ src/main/java/math/TaylorSeries.java
@@ -101,4 +101,23 @@ public class TaylorSeries {
         }
         return sum;
     }
+
+    public static double arctg(double x, int n){
+        if (x > 1 || x < -1){
+            throw new IllegalArgumentException("X should be less than 1 or more than -1");
+        }
+        double sum = 0;
+        double numerator = 1;
+        double denominator = 0;
+        double sign = 1;
+        for (int i = 1; i <= 2 * n - 1; i++) {
+            numerator *= x;
+            denominator++;
+            if (i % 2 != 0){
+                sum += numerator / denominator * sign;
+                sign = -sign;
+            }
+        }
+        return sum;
+    }
 }
